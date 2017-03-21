496fa98cfa85b8b2883e4b9fd9e773853344c6cd
yzbx <youdaoyzbx@163.com>
2017-03-21 21:39:24 +0800

    hahaha

---
+++ scripts/demo-ioucheck.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from scripts.yolo_video import YOLO_VIDEO
+yolo = YOLO_VIDEO()
+
+yolo.disp_console = True
+yolo.imshow = True
+yolo.tofile_video = 'test/output.avi'
+yolo.tofile_txt = 'test/output.txt'
+yolo.filewrite_img = True
+yolo.filewrite_txt = True
+
+yolo.detect_from_file('/media/sdb/CVDataset/UrbanTracker/rouen_video.avi')
--- scripts/iou.py
+++ scripts/iou.py
@@ -4,6 +4,15 @@ import csv
 import cv2
 
 def checkrule(rule,test):
+    """
+        return true if rule passed
+        return false if rule not passed, which means overlap with the same type
+        eg: rule=['dog',100,200,30,30,0.2]
+        note: currently rule[5]=0.2 is not used!
+    """
+    if(rule[0]!=test[0]):
+        return True
+
     rect1=rule[1:5]
     rect2=test[1:5]
     rect1[2]+=rect1[0]
@@ -20,35 +29,36 @@ def checkrule(rule,test):
     else:
         return False
 
-rule=['dog',100,200,30,30,0.2]
+if __name__ == '__main__':
+    rule=['dog',100,200,30,30,0.2]
 
-img_cp=cv2.imread('test/a.jpg')
-x=rule[1]
-y=rule[2]
-w=rule[3]
-h=rule[4]
+    img_cp=cv2.imread('test/a.jpg')
+    x=rule[1]
+    y=rule[2]
+    w=rule[3]
+    h=rule[4]
 
-cv2.rectangle(img_cp, (x - w, y - h), (x + w, y + h), (0, 0, 255), 2)
-cv2.rectangle(img_cp, (x - w, y - h - 20), (x + w, y - h), (125, 125, 125), -1)
-cv2.putText(img_cp, 'no'+rule[0] + ' : %.2f' % rule[5], (x - w + 5, y - h - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        (0, 0, 0), 1)
+    cv2.rectangle(img_cp, (x - w, y - h), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(img_cp, (x - w, y - h - 20), (x + w, y - h), (125, 125, 125), -1)
+    cv2.putText(img_cp, 'no'+rule[0] + ' : %.2f' % rule[5], (x - w + 5, y - h - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+            (0, 0, 0), 1)
 
-cv2.imshow('YOLO_small detection',img_cp)
-cv2.waitKey(0)
+    cv2.imshow('YOLO_small detection',img_cp)
+    cv2.waitKey(0)
 
-csvfile = file('test/a.txt', 'r')
-reader = csv.reader(csvfile)
+    csvfile = file('test/a.txt', 'r')
+    reader = csv.reader(csvfile)
 
-for line in reader:
-    print line
-    line[1]=int(line[1])
-    line[2]=int(line[2])
-    line[3]=int(line[3])
-    line[4]=int(line[4])
-    line[5]=float(line[5])
-    if(checkrule(rule,line)):
-        print "not overlap"
-    else:
-        print "overlap"
+    for line in reader:
+        print line
+        line[1]=int(line[1])
+        line[2]=int(line[2])
+        line[3]=int(line[3])
+        line[4]=int(line[4])
+        line[5]=float(line[5])
+        if(checkrule(rule,line)):
+            print "not overlap"
+        else:
+            print "overlap"
 
-csvfile.close()
+    csvfile.close()

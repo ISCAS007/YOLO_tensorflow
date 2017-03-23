a1e51faacaee096d3554fc7973d3f6ff58cb1302
yzbx <youdaoyzbx@163.com>
2017-03-23 11:56:45 +0800

    lalala

--- scripts/yolo_video.py
+++ scripts/yolo_video.py
@@ -262,7 +262,7 @@ class YOLO_VIDEO:
 
         cv2.rectangle(img, (x - w, y - h), (x + w, y + h), (0, 0, 255), 2)
         cv2.rectangle(img, (x - w, y - h - 20), (x + w, y - h), (125, 125, 125), -1)
-        cv2.putText(img, 'no' + self.rule[0] + ' : %.2f' % self[5], (x - w + 5, y - h - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+        cv2.putText(img, 'no' + self.rule[0] + ' : %.2f' % self.rule[5], (x - w + 5, y - h - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     (0, 0, 0), 1)
 
         return img
